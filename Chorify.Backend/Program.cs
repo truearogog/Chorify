6bc88c9ccd64183234d1c2db5fb2834175c448fb
Artjom Strelkov <artoxaaa5@gmail.com>
2023-03-08 21:21:12 +0200

    Revert "Added fallback"
    This reverts commit c87b0e15b2203a02aae61905d3cf7877dff187e8.

--- Chorify.Backend/Program.cs
+++ Chorify.Backend/Program.cs
@@ -39,15 +39,14 @@ namespace Chorify.Backend
 
             app.UseAuthorization();
 
+            app.MapControllers();
+
             app.UseCors(options => options
                 .WithOrigins(new[] { "http://localhost:3000" })
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
             );
-
-            app.MapControllers();
-            app.MapFallbackToController("Index", "Fallback");
 
             app.Run();
         }
